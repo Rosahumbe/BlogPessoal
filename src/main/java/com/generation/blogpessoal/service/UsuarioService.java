ef6f7111ca5861d96ad746eccc1a6e65e40f096b
Rosa <humbebrito@yahoo.com>
2022-07-14 17:55:47 -0300

    Atualização do security service e do pomxml

---
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.generation.blogpessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.generation.blogpessoal.model.UserLogin;
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario CadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+		
+		String senhaEnconder = enconder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEnconder);
+		
+		return repository.save(usuario);
+	}
+	
+	public  Optional<UserLogin> Logar(Optional<UserLogin> user){
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(enconder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encondedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader ="Basic " + new String(encondedAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				
+				return user;
+				
+			}
+		}
+		
+		return null;
+	
+		
+	}
+
+}
