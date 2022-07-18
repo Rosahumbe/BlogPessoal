ffc9addc2ef20ea77f7148249c2dfde71f3a3c97
Rosa <humbebrito@yahoo.com>
2022-07-18 20:14:14 -0300

    Swagger para deploy

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -9,36 +9,43 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	@NotNull(message = "O atributo nome é obrigatório")
-	private String nome;
-	
-	@NotNull(message = "O atributo usuário é obrigatório")
-	private String usuario;
-	
-	@NotNull(message = "O atributo senha é obrigatório")
-	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
-	private String senha;
-	
-    private String foto;
-    
-	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("usuario")
-	private List <Postagem> postagem;
+	private Long id;
 
-	// Primeiro método Construtor
+	@NotNull(message = "O atributo Nome é Obrigatório!")
+	private String nome;
+
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	private String usuario;
+
+	@NotBlank(message = "O atributo Senha é Obrigatório!")
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	private String senha;
+
+	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+	private String foto;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	/* Métodos Construtores  */
 
 	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
@@ -47,17 +54,17 @@ public class Usuario {
 		this.senha = senha;
 		this.foto = foto;
 	}
+	
+	public Usuario() { }
+	
+	/* Insira os Getters and Setters */
 
-	// Segundo método Construtor
 
-	public Usuario() {	}
-
-
-	public long getId() {
+	public Long getId() {
 		return this.id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -85,14 +92,14 @@ public class Usuario {
 		this.senha = senha;
 	}
 
-    public String getFoto() {
-		return foto;
+	public String getFoto() {
+		return this.foto;
 	}
 
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-    
+
 	public List<Postagem> getPostagem() {
 		return this.postagem;
 	}
